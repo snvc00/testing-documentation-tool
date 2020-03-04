b4115a16ab1fc396ed0d0bda91b07a187246d2d2
Santiago Valle <snvc00@gmail.com>
2020-03-04 13:02:51 -0600

    Add files via upload

---
+++ TestingDocumentationTool/MainForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TestingDocumentationTool
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.ButtonSetupTestPlan = new System.Windows.Forms.Button();
+            this.LabelAbout = new System.Windows.Forms.Label();
+            this.ButtonLinkedIn = new System.Windows.Forms.Button();
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.ButtonMinimize = new System.Windows.Forms.Button();
+            this.ButtonTestCases = new System.Windows.Forms.Button();
+            this.ButtonSummary = new System.Windows.Forms.Button();
+            this.ButtonResults = new System.Windows.Forms.Button();
+            this.ButtonDownloadXLS = new System.Windows.Forms.Button();
+            this.ButtonGitHub = new System.Windows.Forms.Button();
+            this.LabelWindowLightMode = new System.Windows.Forms.Label();
+            this.ButtonWModeBackground = new System.Windows.Forms.Button();
+            this.ButtonWModeForeground = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonSetupTestPlan
+            // 
+            this.ButtonSetupTestPlan.FlatAppearance.BorderSize = 0;
+            this.ButtonSetupTestPlan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonSetupTestPlan.Image = ((System.Drawing.Image)(resources.GetObject("ButtonSetupTestPlan.Image")));
+            this.ButtonSetupTestPlan.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ButtonSetupTestPlan.Location = new System.Drawing.Point(80, 85);
+            this.ButtonSetupTestPlan.Name = "ButtonSetupTestPlan";
+            this.ButtonSetupTestPlan.Padding = new System.Windows.Forms.Padding(0, 10, 0, 20);
+            this.ButtonSetupTestPlan.Size = new System.Drawing.Size(152, 161);
+            this.ButtonSetupTestPlan.TabIndex = 0;
+            this.ButtonSetupTestPlan.Text = "Setup Test Plan";
+            this.ButtonSetupTestPlan.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ButtonSetupTestPlan.UseVisualStyleBackColor = true;
+            this.ButtonSetupTestPlan.Click += new System.EventHandler(this.ButtonSetupTestPlan_Click);
+            // 
+            // LabelAbout
+            // 
+            this.LabelAbout.AutoSize = true;
+            this.LabelAbout.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelAbout.Location = new System.Drawing.Point(151, 685);
+            this.LabelAbout.Name = "LabelAbout";
+            this.LabelAbout.Size = new System.Drawing.Size(130, 56);
+            this.LabelAbout.TabIndex = 5;
+            this.LabelAbout.Text = "ABOUT THE \r\nDEVELOPER";
+            this.LabelAbout.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ButtonLinkedIn
+            // 
+            this.ButtonLinkedIn.FlatAppearance.BorderSize = 0;
+            this.ButtonLinkedIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonLinkedIn.Image = ((System.Drawing.Image)(resources.GetObject("ButtonLinkedIn.Image")));
+            this.ButtonLinkedIn.Location = new System.Drawing.Point(274, 682);
+            this.ButtonLinkedIn.Name = "ButtonLinkedIn";
+            this.ButtonLinkedIn.Size = new System.Drawing.Size(42, 48);
+            this.ButtonLinkedIn.TabIndex = 6;
+            this.ButtonLinkedIn.UseVisualStyleBackColor = true;
+            this.ButtonLinkedIn.Click += new System.EventHandler(this.ButtonLinkedIn_Click);
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.FlatAppearance.BorderSize = 0;
+            this.ButtonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonClose.Image = ((System.Drawing.Image)(resources.GetObject("ButtonClose.Image")));
+            this.ButtonClose.Location = new System.Drawing.Point(1183, 0);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(37, 30);
+            this.ButtonClose.TabIndex = 12;
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // ButtonMinimize
+            // 
+            this.ButtonMinimize.FlatAppearance.BorderSize = 0;
+            this.ButtonMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonMinimize.Image = ((System.Drawing.Image)(resources.GetObject("ButtonMinimize.Image")));
+            this.ButtonMinimize.Location = new System.Drawing.Point(1146, -5);
+            this.ButtonMinimize.Name = "ButtonMinimize";
+            this.ButtonMinimize.Size = new System.Drawing.Size(31, 33);
+            this.ButtonMinimize.TabIndex = 14;
+            this.ButtonMinimize.UseVisualStyleBackColor = true;
+            this.ButtonMinimize.Click += new System.EventHandler(this.ButtonMinimize_Click);
+            // 
+            // ButtonTestCases
+            // 
+            this.ButtonTestCases.FlatAppearance.BorderSize = 0;
+            this.ButtonTestCases.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonTestCases.Image = ((System.Drawing.Image)(resources.GetObject("ButtonTestCases.Image")));
+            this.ButtonTestCases.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ButtonTestCases.Location = new System.Drawing.Point(274, 85);
+            this.ButtonTestCases.Name = "ButtonTestCases";
+            this.ButtonTestCases.Padding = new System.Windows.Forms.Padding(0, 10, 0, 20);
+            this.ButtonTestCases.Size = new System.Drawing.Size(153, 161);
+            this.ButtonTestCases.TabIndex = 15;
+            this.ButtonTestCases.Text = "Test Cases";
+            this.ButtonTestCases.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ButtonTestCases.UseVisualStyleBackColor = true;
+            this.ButtonTestCases.Click += new System.EventHandler(this.ButtonTestCases_Click);
+            // 
+            // ButtonSummary
+            // 
+            this.ButtonSummary.FlatAppearance.BorderSize = 0;
+            this.ButtonSummary.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonSummary.Image = ((System.Drawing.Image)(resources.GetObject("ButtonSummary.Image")));
+            this.ButtonSummary.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ButtonSummary.Location = new System.Drawing.Point(274, 284);
+            this.ButtonSummary.Name = "ButtonSummary";
+            this.ButtonSummary.Padding = new System.Windows.Forms.Padding(0, 10, 0, 20);
+            this.ButtonSummary.Size = new System.Drawing.Size(153, 161);
+            this.ButtonSummary.TabIndex = 17;
+            this.ButtonSummary.Text = "Summary";
+            this.ButtonSummary.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ButtonSummary.UseVisualStyleBackColor = true;
+            this.ButtonSummary.Click += new System.EventHandler(this.ButtonSummary_Click);
+            // 
+            // ButtonResults
+            // 
+            this.ButtonResults.FlatAppearance.BorderSize = 0;
+            this.ButtonResults.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonResults.Image = ((System.Drawing.Image)(resources.GetObject("ButtonResults.Image")));
+            this.ButtonResults.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ButtonResults.Location = new System.Drawing.Point(80, 284);
+            this.ButtonResults.Name = "ButtonResults";
+            this.ButtonResults.Padding = new System.Windows.Forms.Padding(0, 10, 0, 20);
+            this.ButtonResults.Size = new System.Drawing.Size(152, 161);
+            this.ButtonResults.TabIndex = 16;
+            this.ButtonResults.Text = "Results";
+            this.ButtonResults.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ButtonResults.UseVisualStyleBackColor = true;
+            this.ButtonResults.Click += new System.EventHandler(this.ButtonResults_Click);
+            // 
+            // ButtonDownloadXLS
+            // 
+            this.ButtonDownloadXLS.FlatAppearance.BorderSize = 0;
+            this.ButtonDownloadXLS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonDownloadXLS.Image = ((System.Drawing.Image)(resources.GetObject("ButtonDownloadXLS.Image")));
+            this.ButtonDownloadXLS.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.ButtonDownloadXLS.Location = new System.Drawing.Point(80, 483);
+            this.ButtonDownloadXLS.Name = "ButtonDownloadXLS";
+            this.ButtonDownloadXLS.Padding = new System.Windows.Forms.Padding(0, 10, 0, 20);
+            this.ButtonDownloadXLS.Size = new System.Drawing.Size(153, 161);
+            this.ButtonDownloadXLS.TabIndex = 18;
+            this.ButtonDownloadXLS.Text = "Download XLS FIle";
+            this.ButtonDownloadXLS.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ButtonDownloadXLS.UseVisualStyleBackColor = true;
+            this.ButtonDownloadXLS.Click += new System.EventHandler(this.ButtonDownloadXLS_Click);
+            // 
+            // ButtonGitHub
+            // 
+            this.ButtonGitHub.FlatAppearance.BorderSize = 0;
+            this.ButtonGitHub.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonGitHub.Image = ((System.Drawing.Image)(resources.GetObject("ButtonGitHub.Image")));
+            this.ButtonGitHub.Location = new System.Drawing.Point(331, 683);
+            this.ButtonGitHub.Name = "ButtonGitHub";
+            this.ButtonGitHub.Size = new System.Drawing.Size(42, 48);
+            this.ButtonGitHub.TabIndex = 19;
+            this.ButtonGitHub.UseVisualStyleBackColor = true;
+            this.ButtonGitHub.Click += new System.EventHandler(this.ButtonGitHub_Click);
+            // 
+            // LabelWindowLightMode
+            // 
+            this.LabelWindowLightMode.AutoSize = true;
+            this.LabelWindowLightMode.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelWindowLightMode.Location = new System.Drawing.Point(923, 701);
+            this.LabelWindowLightMode.Name = "LabelWindowLightMode";
+            this.LabelWindowLightMode.Size = new System.Drawing.Size(161, 32);
+            this.LabelWindowLightMode.TabIndex = 20;
+            this.LabelWindowLightMode.Text = "DARK MODE";
+            // 
+            // ButtonWModeBackground
+            // 
+            this.ButtonWModeBackground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ButtonWModeBackground.FlatAppearance.BorderSize = 0;
+            this.ButtonWModeBackground.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonWModeBackground.Location = new System.Drawing.Point(1062, 697);
+            this.ButtonWModeBackground.Name = "ButtonWModeBackground";
+            this.ButtonWModeBackground.Size = new System.Drawing.Size(62, 32);
+            this.ButtonWModeBackground.TabIndex = 21;
+            this.ButtonWModeBackground.UseVisualStyleBackColor = false;
+            this.ButtonWModeBackground.Click += new System.EventHandler(this.ButtonWModeBackground_Click);
+            // 
+            // ButtonWModeForeground
+            // 
+            this.ButtonWModeForeground.BackColor = System.Drawing.Color.White;
+            this.ButtonWModeForeground.FlatAppearance.BorderSize = 0;
+            this.ButtonWModeForeground.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonWModeForeground.Location = new System.Drawing.Point(1063, 698);
+            this.ButtonWModeForeground.Name = "ButtonWModeForeground";
+            this.ButtonWModeForeground.Size = new System.Drawing.Size(30, 30);
+            this.ButtonWModeForeground.TabIndex = 22;
+            this.ButtonWModeForeground.UseVisualStyleBackColor = false;
+            this.ButtonWModeForeground.Click += new System.EventHandler(this.ButtonWModeForeground_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1220, 767);
+            this.Controls.Add(this.ButtonWModeForeground);
+            this.Controls.Add(this.ButtonWModeBackground);
+            this.Controls.Add(this.LabelWindowLightMode);
+            this.Controls.Add(this.ButtonGitHub);
+            this.Controls.Add(this.ButtonDownloadXLS);
+            this.Controls.Add(this.ButtonSummary);
+            this.Controls.Add(this.ButtonResults);
+            this.Controls.Add(this.ButtonTestCases);
+            this.Controls.Add(this.ButtonMinimize);
+            this.Controls.Add(this.ButtonClose);
+            this.Controls.Add(this.ButtonLinkedIn);
+            this.Controls.Add(this.LabelAbout);
+            this.Controls.Add(this.ButtonSetupTestPlan);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonSetupTestPlan;
+        private System.Windows.Forms.Label LabelAbout;
+        private System.Windows.Forms.Button ButtonLinkedIn;
+        private System.Windows.Forms.Button ButtonClose;
+        private System.Windows.Forms.Button ButtonMinimize;
+        private System.Windows.Forms.Button ButtonTestCases;
+        private System.Windows.Forms.Button ButtonSummary;
+        private System.Windows.Forms.Button ButtonResults;
+        private System.Windows.Forms.Button ButtonDownloadXLS;
+        private System.Windows.Forms.Button ButtonGitHub;
+        private System.Windows.Forms.Label LabelWindowLightMode;
+        private System.Windows.Forms.Button ButtonWModeBackground;
+        private System.Windows.Forms.Button ButtonWModeForeground;
+    }
+}
+
---
+++ TestingDocumentationTool/MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TestingDocumentationTool
+{
+    public partial class MainWindow : Form
+    {
+        string AvailableLightMode;
+        public MainWindow()
+        {
+            InitializeComponent();
+            AvailableLightMode = "DARK MODE";
+            LabelWindowLightMode.Text = AvailableLightMode;
+        }
+
+        // Window Control Buttons
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        // App Functionality Buttons
+
+        private void ButtonSetupTestPlan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonTestCases_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonResults_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonSummary_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonDownloadXLS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Extra Buttons
+
+        private void ButtonLinkedIn_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.linkedin.com/in/santiago-valle-463130194/");
+        }
+
+        private void ButtonGitHub_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/snvc00");
+        }
+    
+
+        private void ButtonWModeForeground_Click(object sender, EventArgs e)
+        {
+            WindowMode();
+        }
+
+        private void ButtonWModeBackground_Click(object sender, EventArgs e)
+        {
+            WindowMode();
+        }
+
+        private void WindowMode()
+        {
+            if (AvailableLightMode == "DARK MODE")
+            {
+                // Dark Mode Setup
+                DarkMode();
+                ButtonWModeBackground.BackColor = Color.LightGreen;
+                ButtonWModeForeground.Location = new Point(ButtonWModeForeground.Location.X + 30, ButtonWModeForeground.Location.Y);
+                AvailableLightMode = "LIGHT MODE";
+            }
+            else
+            {
+                // Light Mode Setup
+                LightMode();
+                ButtonWModeBackground.BackColor = Color.LightGray;
+                ButtonWModeForeground.Location = new Point(ButtonWModeForeground.Location.X - 30, ButtonWModeForeground.Location.Y);
+                AvailableLightMode = "DARK MODE";
+            }
+        }
+
+        private void DarkMode()
+        {
+            BackColor = Color.FromArgb(40, 40, 40);
+            LabelAbout.ForeColor = Color.White;
+            LabelWindowLightMode.ForeColor = Color.White;
+            ButtonSetupTestPlan.ForeColor = Color.White;
+            ButtonTestCases.ForeColor = Color.White;
+            ButtonResults.ForeColor = Color.White;
+            ButtonSummary.ForeColor = Color.White;
+
+            ButtonDownloadXLS.ForeColor = Color.White;
+            ButtonSetupTestPlan.Image = Image.FromFile("..\\..\\..\\static\\setupDarkMode.png");
+            ButtonTestCases.Image = Image.FromFile("..\\..\\..\\static\\testDarkMode.png");
+            ButtonResults.Image = Image.FromFile("..\\..\\..\\static\\resultsDarkMode.png");
+            ButtonSummary.Image = Image.FromFile("..\\..\\..\\static\\summaryDarkMode.png");
+            ButtonDownloadXLS.Image = Image.FromFile("..\\..\\..\\static\\downloadXlsDarkMode.png");
+
+            ButtonClose.Image = Image.FromFile("..\\..\\..\\static\\closeWDarkMode.png");
+            ButtonMinimize.Image = Image.FromFile("..\\..\\..\\static\\minimizeDarkMode.png");
+            ButtonLinkedIn.Image = Image.FromFile("..\\..\\..\\static\\linkedinDarkMode.png");
+            ButtonGitHub.Image = Image.FromFile("..\\..\\..\\static\\githubDarkMode.png");
+
+            Refresh();
+        }
+
+        private void LightMode()
+        {
+            BackColor = Color.White;
+            LabelAbout.ForeColor = Color.Black;
+            LabelWindowLightMode.ForeColor = Color.Black;
+            ButtonSetupTestPlan.ForeColor = Color.Black;
+            ButtonTestCases.ForeColor = Color.Black;
+            ButtonResults.ForeColor = Color.Black;
+            ButtonSummary.ForeColor = Color.Black;
+            ButtonDownloadXLS.ForeColor = Color.Black;
+
+            ButtonSetupTestPlan.Image = Image.FromFile("..\\..\\..\\static\\setup.png");
+            ButtonTestCases.Image = Image.FromFile("..\\..\\..\\static\\test.png");
+            ButtonResults.Image = Image.FromFile("..\\..\\..\\static\\results.png");
+            ButtonSummary.Image = Image.FromFile("..\\..\\..\\static\\summary.png");
+            ButtonDownloadXLS.Image = Image.FromFile("..\\..\\..\\static\\downloadXls.png");
+
+            ButtonClose.Image = Image.FromFile("..\\..\\..\\static\\closeW.png");
+            ButtonMinimize.Image = Image.FromFile("..\\..\\..\\static\\minimize.png");
+            ButtonLinkedIn.Image = Image.FromFile("..\\..\\..\\static\\linkedin.png");
+            ButtonGitHub.Image = Image.FromFile("..\\..\\..\\static\\github.png");
+
+            Refresh();
+        }
+    }
+}
